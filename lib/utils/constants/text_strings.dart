704a8edbbb019bd8c3c299b271294351b6172e4d
SandevD <sandev.net@gmail.com>
2023-12-25 16:34:03 +0530

    🎨Add text strings for onboarding and home app bar

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,12 @@
+class ETexts {
+  static const String onBoardingTitle1 = 'Your Product, Your Choice - Make It Now!';
+  static const String onBoardingTitle2 = 'Select Payment Method';
+  static const String onBoardingTitle3 = 'Doorstep Delivery, Just for You!';
+  
+  static const String onBoardingSubTitle1 = 'Step into a Realm of Infinite Options - Your Ideal Product Awaits!';
+  static const String onBoardingSubTitle2 = 'Your Convenience, Our Priority! Optimize Your Payment Path for Seamless Transactions.';
+  static const String onBoardingSubTitle3 = 'Seamless Delivery, Ultimate Enjoyment - From Our Doorstep to Yours!';
+
+  static const String homeAppBarTitle = 'Shop on this Fine Day!';
+  static const String homeAppBarSubTitle = 'Sandev Dullewa';
+}
